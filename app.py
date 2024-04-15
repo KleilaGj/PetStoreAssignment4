6ec19eace8fe3cdd0a20b5576cbf929afc81c1c9
tahirahchinyere <162932410+tahirahchinyere@users.noreply.github.com>
2024-04-15 22:31:32 +0100

    Update app.py
    added comments to the code on the steps and processes

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, request
-#adding new code to intergrate flask aplication with database utilities 
+# Code to intergrate flask aplication with database utilities 
 from db_utils import connect_to_database, execute_query
 app = Flask(__name__)
-#adding code to connect to the database 
+# initialize database connection 
 db_connection = connect_to_database()
-#modifiying current code 
+# defne a route to retrieve all pets from the database 
 @app.route('/pets', methods=['GET'])
 def get_pets():
-    #getting pets data from database 
+    # construct a SQL query to select all pets from the database 
     query = "SELECT * FROM Pets_info"
+    # Execute the SQL query and retrieve pets data 
     pets = execute_query(db_connection, query)
+    # Return JSON response containing pets data 
     return jsonify(pets)
 # Modified route to support both GET and PUT methods
 @app.route('/pets/<int:pet_id>', methods=['GET', 'PUT'])
@@ -17,15 +19,19 @@ def get_or_update_pet(pet_id):
     if request.method == 'GET':
         # SELECT query to fetch a specific pet by ID
         query = "SELECT * FROM Pets_info WHERE pet_id = %s"
+        # provide the pet ID as data for the query
         data = (pet_id,)
+        #Execute the SQL query and retrieve the pet data 
         pet = execute_query(db_connection, query, data)
+        # check if the pet exists and return JSON response accordingly 
         if pet:
             return jsonify(pet)
         else:
             return jsonify({"message": "Pet not found"}), 404
     elif request.method == 'PUT':
-        # Update pet information
+        # retieve JSON data from the request body 
         data = request.get_json()
+        # Extract information for updating the pet 
         new_name = data.get('name')
         new_breed = data.get('breed')
         new_age = data.get('age')
@@ -34,13 +40,18 @@ def get_or_update_pet(pet_id):
         # UPDATE query to modify pet's information in the database
         update_query = "UPDATE Pets_info SET name=%s, breed=%s, age=%s, price=%s WHERE pet_id=%s"
         update_data = (new_name, new_breed, new_age, new_price, pet_id)
+        #Execute the SQL query to update the pet's information 
         execute_query(db_connection, update_query, update_data)
-
+        
+        #return JSON response indicating successful updating 
         return jsonify({"message": "Pet information updated successfully"}), 200
 #define a function to start the Flask application with a welcome statement 
 def run():
     print("Welcome to the Pet Store API!")
     app.run(debug=True)
+    #check if the script is executed directly
 if __name__ == '__main__':
+    # call the run function to start the Flask application 
+    run()
     
 
